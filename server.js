608c098f6c80dee551926b8001c0a717d2d67deb
hanzo7656-prog <hanzo7656@gmail.com>
2025-10-05 10:13:22 +0330

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const logger = winston.createLogger({
 });
 
 // ==================== API کلیدها ====================
-const COINSTATS_API_KEY = "7qmXYUHIF+DWnF9fYml4Klz+/leL7EBRH+mA2WrpsEc=";
+const COINSTATS_API_KEY = "uNb+sOjnjCQmV30dYrChxgh55hRHElmiZLnKJX+5U6g=";
 
 // ==================== میدلورها ====================
 app.use(cors());
@@ -47,7 +47,40 @@ let cache = {
 const ALL_TRADING_PAIRS = [
     "btc_usdt", "eth_usdt", "xrp_usdt", "ada_usdt", "dot_usdt", "doge_usdt", "sol_usdt", "matic_usdt",
     "avax_usdt", "link_usdt", "bch_usdt", "ltc_usdt", "etc_usdt", "trx_usdt", "atom_usdt", "bnb_usdt",
-    // ... تمام 200+ ارز از سرور قدیمی
+    "xim_usdt", "eos_usdt", "xtz_usdt", "algo_usdt", "neo_usdt", "ftm_usdt", "hbar_usdt", "egld_usdt",
+    "theta_usdt", "vet_usdt", "fil_usdt", "icp_usdt", "xmr_usdt", "ape_usdt", "gala_usdt", "sand_usdt",
+    "mana_usdt", "enj_usdt", "bat_usdt", "comp_usdt", "mkr_usdt", "zec_usdt", "dash_usdt",
+    "waves_usdt", "omp_usdt", "zil_usdt", "ont_usdt", "ost_usdt", "stx_usdt", "celo_usdt", "wn_usdt",
+    "sc_usdt", "zen_usdt", "hot_usdt", "iotx_usdt", "ong_usdt", "one_usdt", "nano_usdt", "ardr_usdt",
+    "qtum_usdt", "lsk_usdt", "strat_usdt", "kmd_usdt", "piyx_usdt", "grs_usdt", "nav_usdt", "emo2_usdt",
+    "xyg_usdt", "sys_usdt", "via_usdt", "mona_usdt", "dcr_usdt", "sia_usdt", "lbc_usdt", "rep_usdt",
+    "gnt_usdt", "loom_usdt", "poly_usdt", "ren_usdt", "fun_usdt", "reg_usdt", "salt_usdt", "mtl_usdt",
+    "mco_usdt", "edo_usdt", "powr_usdt", "irc_usdt", "gio_usdt", "eng_usdt", "ast_usdt", "dgd_usdt",
+    "adx_usdt", "qsp_usdt", "mda_usdt", "snt_usdt", "agix_usdt", "ocean_usdt", "band_usdt",
+    "mmr_usdt", "ric_usdt", "uos_usdt", "stopi_usdt", "keep_usdt", "om_usdt", "front_usdt", "perp_usdt",
+    "api3_usdt", "grt_usdt", "lqty_usdt", "alcx_usdt", "pool_usdt", "rad_usdt", "farm_usdt", "audio_usdt",
+    "fs_usdt", "dodo_usdt", "tim_usdt", "liv_usdt", "ygg_usdt", "slp_usdt", "axs_usdt", "sandbox_usdt",
+    "enjin_usdt", "mdr_usdt", "flow_usdt", "rose_usdt", "ar_usdt", "rune_usdt", "sushi_usdt", "crv_usdt",
+    "linch_usdt", "knc_usdt", "bal_usdt", "uma_usdt", "badger_usdt", "fxs_usdt", "cvx_usdt",
+    "tribe_usdt", "gno_usdt", "ilus_usdt", "pla_usdt", "super_usdt", "ach_usdt", "imx_usdt", "gods_usdt",
+    "vra_usdt", "sps_usdt", "dar_usdt", "mgp_usdt", "ceek_usdt", "ttian_usdt", "vr_usdt", "bmx_usdt",
+    "hero_usdt", "pyr_usdt", "ufo_usdt", "elon_usdt", "shib_usdt", "floki_usdt", "samo_usdt",
+    "baby_usdt", "kishu_usdt", "hoge_usdt", "akita_usdt", "husky_usdt", "safernoon_usdt",
+    "evergrow_usdt", "lunc_usdt", "bonk_usdt", "wif_usdt", "myro_usdt", "popcat_usdt", "toshi_usdt",
+    "mew_usdt", "mog_usdt", "rett_usdt", "turbo_usdt", "pepe_usdt", "wojak_usdt", "aidos_usdt",
+    "pudgy_usdt", "lady_usdt", "jeo_usdt", "based_usdt", "degen_usdt", "moutai_usdt", "aave_usdt",
+    "snx_usdt", "uni_usdt", "cake_usdt", "bake_usdt", "burger_usdt", "toko_usdt", "inj_usdt", "lina_usdt",
+    "reef_usdt", "dusk_usdt", "atm_usdt", "ogn_usdt", "for_usdt", "mir_usdt", "ito_usdt", "cos_usdt",
+    "ctk_usdt", "tko_usdt", "alpaca_usdt", "perl_usdt", "stpt_usdt", "troy_usdt", "vite_usdt", "svp_usdt",
+    "hbtc_usdt", "mdt_usdt", "mbox_usdt", "gmt_usdt", "time_usdt", "raca_usdt", "beans_usdt",
+    "edu_usdt", "id_usdt", "ondo_usdt", "pixel_usdt", "voxel_usdt", "high_usdt", "looks_usdt",
+    "blur_usdt", "psp_usdt", "oxt_usdt", "num_usdt", "mask_usdt", "glm_usdt", "ant_usdt", "bond_usdt",
+    "fida_usdt", "maps_usdt", "drop_usdt", "px_usdt", "clv_usdt", "cfx_usdt", "ckb_usdt", "mx_usdt",
+    "ceir_usdt", "ret_usdt", "stmx_usdt", "chz_usdt", "ankr_usdt", "coti_usdt", "skl_usdt", "arpa_usdt",
+    "strax_usdt", "fo_usdt", "mbl_usdt", "quick_usdt", "sfund_usdt", "bsw_usdt", "axie_usdt",
+    "tfuel_usdt", "hnt_usdt", "loka_usdt", "dydx_usdt", "pundix_usdt", "who_usdt", "dent_usdt",
+    "rsr_usdt", "cvc_usdt", "data_usdt", "nkn_usdt", "lit_usdt", "key_usdt", "dock_usdt", "phb_usdt",
+    "mxc_usdt", "front_usdt"
 ];
 
 // ==================== کلاس Gist Manager از سرور قدیمی ====================
