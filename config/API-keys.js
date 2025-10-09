1ec556ad80b323fc2ae7fc4544d4975f58a44c29
hanzo7656-prog <hanzo7656@gmail.com>
2025-10-10 01:05:11 +0330

    Delete config/API-keys.js

--- config/API-keys.js
+++
@@ -1,6 +0,0 @@
-// config/api-keys.js
-module.exports = {
-    COINSTATS_API_KEY: "uNb+sOjnjCQmV30dYrChxgh55hRHElmiZLnKJX+5U6g=",
-    GITHUB_TOKEN: process.env.GITHUB_TOKEN || "",
-    GIST_ID: process.env.GIST_ID || ""
-};
