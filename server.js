8af26704197cb8e688e1fb13b20fea00ddf72627
hanzo7656-prog <hanzo7656@gmail.com>
2025-10-09 12:51:25 +0330

    Update server.js

--- server.js
+++ server.js
@@ -407,7 +407,7 @@ class HistoricalDataAPI {
 
     symbolToCoinId(symbol) {
         const symbolMap = {
-            'BTC': 'bitcoin', 'ETH': 'ethereum', 'BNB': 'binancecoin', 'SOL': 'solana',
+            'BTC': 'bitcoin', 'ETH': 'ethereum', 'BNB': 'bnb', 'SOL': 'solana',
             'XRP': 'ripple', 'ADA': 'cardano', 'AVAX': 'avalanche-2', 'DOT': 'polkadot',
             'LINK': 'chainlink', 'MATIC': 'matic-network', 'LTC': 'litecoin', 'BCH': 'bitcoin-cash',
             'ATOM': 'cosmos', 'XLM': 'stellar', 'FIL': 'filecoin', 'HBAR': 'hedera-hashgraph',
