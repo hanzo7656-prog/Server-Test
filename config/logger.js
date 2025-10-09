17c35c5e668b8bed4d956b3d52820316c10507df
hanzo7656-prog <hanzo7656@gmail.com>
2025-10-09 23:31:30 +0330

    Update logger.js

--- config/logger.js
+++ config/logger.js
@@ -6,7 +6,7 @@ const logger = winston.createLogger({
     format: winston.format.combine(
         winston.format.timestamp(),
         winston.format.printf(({ timestamp, level, message }) => {
-            return ${timestamp} - ${level.toUpperCase()} - ${message};
+            return `${timestamp} - ${level.toUpperCase()} - ${message}`;  // ✅ استفاده از backticks
         })
     ),
     transports: [
