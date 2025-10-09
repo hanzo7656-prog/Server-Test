498f09bae3171a208618166b27300317400de133
hanzo7656-prog <hanzo7656@gmail.com>
2025-10-09 19:46:40 +0330

    Create API-keys.js

---
+++ config/API-keys.js
@@ -0,0 +1,6 @@
+// config/api-keys.js
+module.exports = {
+    COINSTATS_API_KEY: "uNb+sOjnjCQmV30dYrChxgh55hRHElmiZLnKJX+5U6g=",
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN || "",
+    GIST_ID: process.env.GIST_ID || ""
+};
