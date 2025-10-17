b3f46079ccf2d5efc97b01d172c410f5769fb1e9
hanzo7656-prog <hanzo7656@gmail.com>
2025-10-17 23:53:09 +0330

    Update APIClients.js

--- models/APIClients.js
+++ models/APIClients.js
@@ -159,7 +159,7 @@ try {
     } catch (error3) {
       console.log('△ Using fallback constants configuration');
       constants = {
-        COINSTATS_API_KEY: process.env.COINSTATS_API_KEY || "uNb+sQjnjCQmV30dYrChxgh55hRHElmizLnKJX+5U6g=",
+        COINSTATS_API_KEY: process.env.COINSTATS_API_KEY || "40QRC4gdyzWIGwsvGkqWtcDOf0bk+FV217KmLxQ/Wmw=",
         API_URLS: {
           base: "https://openapiv1.coinstats.app",
           exchange: "https://openapiv1.coinstats.app/coins/price/exchange",
