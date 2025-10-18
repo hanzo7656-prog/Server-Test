5444a9a68c6c692daf9673f3c5aadc2145d6fd2a
hanzo7656-prog <hanzo7656@gmail.com>
2025-10-19 00:27:05 +0330

    Update modern-pages.js

--- routes/modern-pages.js
+++ routes/modern-pages.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { generateNavigationHTML } = require('./navigation-generation');
+const { generateNavigationHTML } = require('./navigation-generator');
 
 function generateModernPage(title, content, currentPage = 'home') {
     return `
